e80012155622117789a6da8c882dde92219c5601
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-07-08 00:31:10 +0200

    main/meta: moved licenses

--- src/main/kotlin/fr/stardustenterprises/burst/meta/impl/pom/license.kt
+++ src/main/kotlin/fr/stardustenterprises/burst/meta/impl/pom/license.kt
@@ -2,6 +2,9 @@ package fr.stardustenterprises.burst.meta.impl.pom
 
 import fr.stardustenterprises.burst.meta.Data
 import fr.stardustenterprises.burst.property.data
+import org.gradle.kotlin.dsl.provideDelegate
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 /**
  * A POM-compliant License object.
@@ -16,6 +19,7 @@ class LicenseMetadata : Data<PomMetadata>() {
     }
 }
 
+@Suppress("PropertyName")
 class LicenseMetaHolder : Data<PomMetadata>() {
     internal val licenses: MutableList<LicenseMetadata> =
         mutableListOf()
@@ -32,5 +36,59 @@ class LicenseMetaHolder : Data<PomMetadata>() {
 
     override fun mutate(target: PomMetadata) {
         licenses.forEach { it.mutate(target) }
+    }
+
+    // Curated list of most-known-used-popular(?) licenses.
+    // From https://spdx.org/licenses/
+
+    val bsd0 by LicenseDelegate("0BSD")
+    val agpl3 by LicenseDelegate("AGPL-3.0-only")
+    val `agpl3+` by LicenseDelegate("AGPL-3.0-or-later")
+    val apache1 by LicenseDelegate("Apache-1.0")
+    val apache11 by LicenseDelegate("Apache-1.1")
+    val apache2 by LicenseDelegate("Apache-2.0")
+
+    //TODO: add BSD
+    //TODO: add (full) Creative Commons?
+    val ccbyncsa4 by LicenseDelegate("CC-BY-NC-SA-4.0")
+
+    val gpl1 by LicenseDelegate("GPL-1.0-only")
+    val `gpl1+` by LicenseDelegate("GPL-1.0-or-later")
+    val gpl2 by LicenseDelegate("GPL-2.0-only")
+    val `gpl2+` by LicenseDelegate("GPL-2.0-or-later")
+    val gpl3 by LicenseDelegate("GPL-3.0-only")
+    val `gpl3+` by LicenseDelegate("GPL-3.0-or-later")
+    val isc by LicenseDelegate("ISC")
+    val lgpl2 by LicenseDelegate("LGPL-2.0-only")
+    val `lgpl2+` by LicenseDelegate("LGPL-2.0-or-later")
+    val lgpl21 by LicenseDelegate("LGPL-2.1-only")
+    val `lgpl21+` by LicenseDelegate("LGPL-2.1-or-later")
+    val lgpl3 by LicenseDelegate("LGPL-3.0-only")
+    val `lgpl3+` by LicenseDelegate("LGPL-3.0-or-later")
+    val mit by LicenseDelegate("MIT")
+    val mit0 by LicenseDelegate("MIT-0")
+    val mpl1 by LicenseDelegate("MPL-1.0")
+    val mpl11 by LicenseDelegate("MPL-1.1")
+    val mpl2 by LicenseDelegate("MPL-2.0")
+    val osl1 by LicenseDelegate("OSL-1.0")
+    val osl11 by LicenseDelegate("OSL-1.1")
+    val osl2 by LicenseDelegate("OSL-2.0")
+    val osl21 by LicenseDelegate("OSL-2.1")
+    val osl3 by LicenseDelegate("OSL-3.0")
+    val x11 by LicenseDelegate("X11")
+}
+
+class LicenseDelegate(
+    val name: String,
+    val url: String = "https://spdx.org/licenses/$name.html",
+) : ReadOnlyProperty<LicenseMetaHolder, LicenseMetadata> {
+    override fun getValue(thisRef: LicenseMetaHolder, property: KProperty<*>): LicenseMetadata {
+        val metadata = LicenseMetadata().apply {
+            this.name = this@LicenseDelegate.name
+            this.url = this@LicenseDelegate.url
+        }
+
+        thisRef.licenses += metadata
+        return metadata
     }
 }
